2a476ad093cd4d6e649e9395ac1a1499c2dee908
Arun Singh Pundir <101244758+Arunsinghpundir@users.noreply.github.com>
2023-03-23 20:11:21 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import "../styles/App.css";
-import pic from "../NewtonSchool.jpg";
+import React from 'react'
+import '../styles/App.css';
+import pic from '../NewtonSchool.jpg'
 const App = () => {
+
   return (
     <div id="navbar">
       <div>NavBar</div>
-      <img src={pic} width="130px" height="100px" margin="10px" />
+      <img src={pic} alt="avatar img" style={{"width":"130px","height":"100px","margin":"10px"}} />
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
